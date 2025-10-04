665f7d897524b697134b31044ed11b565dc6c423
Christian Rivera <crivera2093@gmail.com>
2025-10-04 17:49:58 -0500

    feat: add query methods for searching books

--- fastapi-bookstore/repositories/BookRepository.py
+++ fastapi-bookstore/repositories/BookRepository.py
@@ -3,3 +3,9 @@ from models.BookModel import BookModel
 
 def get_all_books(db: Session):
     return db.query(BookModel).all()
+
+def find_by_id(db: Session, book_id: int):
+    return  db.query(BookModel).filter(BookModel.id == book_id).first()
+
+def find_by_title(db: Session, title: str):
+    return db.query(BookModel).filter(BookModel.title == title).first()
