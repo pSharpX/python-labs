7a699dd5745a77c482f1d0df363d7ecbdaa9ea70
Christian Rivera <crivera2093@gmail.com>
2026-02-24 23:50:28 -0500

    feat:  enable logs method tracking for search and find books use case.

--- fastapi-bookstore/app/user_cases/find_book.py
+++ fastapi-bookstore/app/user_cases/find_book.py
@@ -1,3 +1,4 @@
+from app.core.metrics import track_execution_time
 from app.domain.entities import Book
 from app.domain.repositories import BookRepository
 
@@ -5,5 +6,6 @@ class FindBookUseCase:
     def __init__(self, repo: BookRepository):
         self.repo = repo
 
+    @track_execution_time
     def execute(self, id: int) -> Book:
         return self.repo.get_by_id(id)
--- fastapi-bookstore/app/user_cases/search_book.py
+++ fastapi-bookstore/app/user_cases/search_book.py
@@ -1,4 +1,6 @@
 from typing import List
+
+from app.core.metrics import track_execution_time
 from app.domain.entities import Book, BookSearchCriteria
 from app.domain.repositories import BookRepository
 
@@ -6,5 +8,6 @@ class SearchBookUseCase:
     def __init__(self, repo: BookRepository):
         self.repo = repo
 
+    @track_execution_time
     def execute(self, criteria: BookSearchCriteria) -> List[Book]:
         return self.repo.search(criteria)
