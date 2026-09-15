ea2586feb1c9810d932e2dda01e140a2b522f504
Christian Rivera <crivera2093@gmail.com>
2026-09-15 14:47:44 -0500

    feat(techdoc-agent): add requirements scout agent.

--- techdoc-agent/agents.py
+++ techdoc-agent/agents.py
@@ -1,12 +1,14 @@
 import warnings
 
 from langchain.agents import create_agent, AgentState
+from langchain.agents.middleware import ToolRetryMiddleware, PIIMiddleware
 from langchain.chat_models import init_chat_model
 from langchain_core.messages import HumanMessage
 from langgraph.checkpoint.memory import InMemorySaver
 
 from config import langfuse_handler
-from prompts import TECHDOC_SYSTEM_PROMPT
+from middlewares import CustomGuardsMiddleware
+from prompts import TECHDOC_SYSTEM_PROMPT, REQ_SCOUT_SYSTEM_PROMPT
 from settings import BaseModelSettings
 from tools import SaveMarkdownTool
 
@@ -19,7 +21,6 @@ warnings.filterwarnings(
 
 class TechDocState(AgentState):
     user_id: str
-
 
 class TechDocAgent:
     def __init__(self):
@@ -75,3 +76,44 @@ class TechDocAgent:
                 context=input_obj,
             )
             print(state["messages"][-1].content)
+
+
+class TechDocReqScoutState(AgentState):
+    user_request: str
+    resources: list[str]
+    requirements: str
+
+class TechDocReqScoutAgent:
+    def __init__(self):
+        self.__model_settings = BaseModelSettings()
+        self.__model = init_chat_model(
+            model=self.__model_settings.model_name,
+            model_provider=self.__model_settings.provider,
+            temperature=self.__model_settings.temperature,
+        )
+        self.__system_prompt = REQ_SCOUT_SYSTEM_PROMPT
+        # noinspection bad-argument-type
+        self.__agent = create_agent(
+            model=self.__model,
+            tools=[
+            ],
+            system_prompt=self.__system_prompt,
+            middleware=[
+                #CustomGuardsMiddleware(),
+                PIIMiddleware("api_key", detector=r"sk-[a-zA-Z0-9]{32}", strategy="block"),
+                PIIMiddleware("credit_card", strategy="mask"),
+                PIIMiddleware("email", strategy="redact"),
+                ToolRetryMiddleware(
+                    max_retries=3,
+                    backoff_factor=2.0,
+                    initial_delay=1.0,
+                ),
+            ],
+            name="techdoc-reqscout-agent",
+            state_schema=TechDocReqScoutState,
+            checkpointer=InMemorySaver()
+        )
+
+    def unwrap(self):
+        return self.__agent
+
